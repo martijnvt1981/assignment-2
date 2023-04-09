35cc4000984746f9486d4185d7f57b013c3ce51d
Martijn van Turnhout <martijn.van.turnhout@pharmapartners.nl>
2023-04-09 16:55:14 +0200

    chore: removed unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LayoutModule } from './modules/custom/layout/layout.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    LayoutModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
